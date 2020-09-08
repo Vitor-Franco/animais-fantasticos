4afd9ea60730163fcc8a248114c249fd979f8da6
Vitor Franco <vitoroliveirafranco@gmail.com>
2020-09-08 09:48:34 -0300

    Fetch bitcoin refatorado

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ import initDropDownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
-import initFetchBitcoin from './modules/fetch-bitcoin.js';
+import fetchBitcoin from './modules/fetch-bitcoin.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="list"] a[href^="#"]');
@@ -27,8 +27,9 @@ tooltip.init();
 
 fetchAnimais('../../animaisapi.json', '.numeros-grid');
 
+fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
+
 initAnimacaoScroll();
 initDropDownMenu();
 initMenuMobile();
 initFuncionamento();
-initFetchBitcoin();
